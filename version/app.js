a377f968ef8c11db660a7d2f3a2af97dfbe87813
basharabulaban <ba.ahmad@gmail.com>
2025-09-04 16:10:53 +0300

    bashar 34

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 // n file
 app.get('/', function (req, res) {
-    response = 'bashar 22.' + '\n';
+    response = 'bashar 34.' + '\n';
 
     //send the response to the client
     //send the response to the client
